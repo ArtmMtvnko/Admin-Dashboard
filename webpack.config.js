881d7efcf13893b78e3ea93a7f5e5a68311508f1
Artem <artemm3note@gmail.com>
2023-07-26 19:07:35 +0300

    basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/scripts/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: 'assets/[name][ext]'
+    },
+    devServer: {
+        port: 4200,
+        open: true,
+        hot: true,
+        compress: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/,
+                type: 'asset/resourse'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    }
+}
