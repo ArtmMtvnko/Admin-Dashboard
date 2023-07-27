850ea221e8110a8146b837a58c493339cb0770a8
Artem <artemm3note@gmail.com>
2023-07-27 22:57:37 +0300

    Use wavesurfer library for audio bar
    Make simple audio bar with using
    wavesyrfer.js library. (First when i use library:3)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,18 @@
 import './../index.html'
 import './../styles/index.scss'
+import urlAudio from './../assets/audio/Saga_1op.mp3'
+import WaveSurfer from 'wavesurfer.js'
+
+const wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#bbbbbb39',
+    progressColor: '#ededed',
+    url: urlAudio,
+    height: 40,
+    barWidth: 5,
+    barRadius: 2
+})
+
+wavesurfer.on('interaction', () => {
+    wavesurfer.play()
+})
--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,10 @@ module.exports = {
             {
                 test: /\.html$/,
                 loader: 'html-loader'
+            },
+            {
+                test: /\.mp3$/,
+                type: 'asset/resource'
             }
         ]
     }
